7f7daa4d5ac0b0d4863a358a2ef37901f4866b0e
Valerii Sitkevich <vsitkevich10@gmail.com>
2023-12-09 13:57:28 +0300

    fix all

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -1,34 +1,37 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+
 plugins {
     java
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "2.7.5"
 }
 
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-config-server")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-configure<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension> {
+configure<DependencyManagementExtension> {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0-RC1")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.8")
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.register<Wrapper>("wrapper") {
+    gradleVersion = "8.2.1"
 }
+
+task("prepareKotlinBuildScriptModel"){}
