d6a8366ca7d94d05001a503a56eae0d4b7bbc325
Valerii Sitkevich <vsitkevich10@gmail.com>
2023-12-09 12:27:29 +0300

    fix ribbon

--- util_service/src/main/java/ru/itmo/soa/util/config/CustomWebConfig.java
+++ util_service/src/main/java/ru/itmo/soa/util/config/CustomWebConfig.java
@@ -1,13 +1,7 @@
 package ru.itmo.soa.util.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,17 +11,5 @@ public class CustomWebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_XML);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new XmlMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .registerModule(new JavaTimeModule());
     }
 }
