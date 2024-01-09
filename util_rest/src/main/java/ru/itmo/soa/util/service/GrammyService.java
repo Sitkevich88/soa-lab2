8c291c3fcff35e862de8c24de2904dac788fb61e
Valerii Sitkevich <vsitkevich10@gmail.com>
2024-01-09 22:15:39 +0300

    wip

--- util_rest/src/main/java/ru/itmo/soa/util/service/GrammyService.java
+++ util_rest/src/main/java/ru/itmo/soa/util/service/GrammyService.java
@@ -1,59 +1,20 @@
 package ru.itmo.soa.util.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import ru.itmo.soa.util.dto.AlbumDTO;
-import ru.itmo.soa.util.dto.AlbumDTO2;
 import ru.itmo.soa.util.dto.MusicBandDTO;
-import ru.itmo.soa.util.dto.NumberOfParticipantsDTO;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 
 @Service
 public class GrammyService {
 
-    @Value("${bands.api.base}")
-    private String BASE_URL;
-
-    private final RestTemplate restTemplate;
-
-    public GrammyService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     public MusicBandDTO addParticipant(long bandId) throws URISyntaxException {
-        //get it
-        var url = BASE_URL + "/musicbands/" + bandId;
-
-
-        var bandEnt = restTemplate.getForEntity(url, MusicBandDTO.class);
-        var band = bandEnt.getBody();
-
-        //update it
-        var changes = new NumberOfParticipantsDTO();
-        changes.setNumberOfParticipants(band.getNumberOfParticipants() + 1);
-
-//        return restTemplate.patchForObject(url, changes, MusicBandDTO.class);
-
-        return restTemplate.exchange(new URI(url),
-                HttpMethod.PATCH,
-                new HttpEntity<>(changes),
-                MusicBandDTO.class).getBody();
+        return null;//todo
     }
 
     public MusicBandDTO addSingle(long bandId, AlbumDTO album) throws URISyntaxException {
-        var url = BASE_URL + "/musicbands/" + bandId;
-        var changes = new AlbumDTO2();
-        changes.setBestAlbum(album);
-
-        return restTemplate.exchange(new URI(url),
-                HttpMethod.PATCH,
-                new HttpEntity<>(changes),
-                MusicBandDTO.class).getBody();
+        return null;//todo
     }
 }
